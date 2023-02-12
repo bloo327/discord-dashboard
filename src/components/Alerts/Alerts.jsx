29f1714f166493c072986014fcef5babdf31cea7
Brendan <bliu32799@gmail.com>
2023-02-11 23:54:13 -0800

    styled alerts

--- src/components/Alerts/Alerts.jsx
+++ src/components/Alerts/Alerts.jsx
@@ -13,8 +13,8 @@ const Alerts = ({ alertSetContent }) => {
                 <FaBell className='mt-3 ml-3' size='20'/>
                 <h1 className='text-xl h-12 p-2 font-bold'>Alerts</h1>
             
-                <div className='friendrq-wrap flex mt-3 bg-gray-700 dark:bg-gray-200 dark:hover:bg-gray-400 w-11 h-6 ml-56 rounded-lg cursor-pointer hover:bg-gray-600 transition-all duration-150 ease-in-out' title='Friend Requests'>
-                    <FaUserFriends className='friendrq-icon ml-1 mt-1 text-gray-400 dark:text-gray-500 drop-shadow-lg transition-all duration-150 ease-in-out' size='15' />
+                <div className='friendrq-wrap' title='Friend Requests'>
+                    <FaUserFriends className='friendrq-icon' size='15' />
                     <p className='text-sm font-bold ml-2 mt-0.5 drop-shadow-lg'>0</p>
                 </div>
             </div>
@@ -51,7 +51,7 @@ const AlertIcon = ({ icon, dataSet }) => {
     }
 
     return (
-        <div className='w-11/12 ml-3 mb-6 each-alert flex items-center justify-between'>
+        <div className={`each-alert w-full ml-3 mb-6 flex items-center`}>
             <FaChevronDown size='12' onClick={handleExpand} className={
                     expanded ? 'transition-all ease-out text-opacity-80 my-auto mr-1 cursor-pointer' 
                     : 'transition-all ease-in -rotate-90 text-opacity-80 my-auto mr-1 cursor-pointer' 
@@ -60,17 +60,22 @@ const AlertIcon = ({ icon, dataSet }) => {
             <button onClick={handleClick} className={`${icon.props.name} alert-icon group cursor-pointer mr-4`} >
                 { icon }
             </button>
-            <h1 className='font-bold'> {icon.props.name}</h1>
+            <h1 className='font-bold '> {icon.props.name}</h1>
             <AlertOptions />
         </div>
     )
 }
 
 const AlertOptions = () => {
+
+    const handleClick = () => {
+        console.log(document.getElementsByClassName('each-alert'))
+    }
+
     return (
-        <div className='bell-check flex w-20 ml-24 justify-between'>
-            <div className='flex justify-center items-center w-9 h-9 rounded-3xl mr-2 bg-gray-700 drop-shadow-lg cursor-pointer hover:bg-gray-600 dark:bg-gray-200 transition-all duration-150'><FaRegBell /></div>
-            <div className='flex justify-center items-center w-9 h-9 rounded-3xl bg-gray-700 drop-shadow-lg cursor-pointer hover:bg-gray-600 dark:bg-gray-200 transition-all duration-150'><FaCheck /></div>
+        <div className='bell-check flex ml-20 justify-between'>
+            <div className='bell-icon flex justify-center items-center w-9 h-9 rounded-3xl mr-2 bg-gray-700 drop-shadow-lg cursor-pointer hover:bg-gray-600 dark:bg-gray-200 dark:hover:bg-gray-400 transition-all duration-150'><FaRegBell /></div>
+            <div className='check-icon flex justify-center items-center w-9 h-9 rounded-3xl bg-gray-700 drop-shadow-lg cursor-pointer hover:bg-gray-600 dark:bg-gray-200 dark:hover:bg-gray-400 transition-all duration-150' onClick={handleClick}><FaCheck /></div>
         </div>
     )
 }
